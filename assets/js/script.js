8cfbba89fa6b0ef2452e4777fcb2b5cf53fb31c3
Andi Gjika <andi.gjika@outlook.com>
2025-08-26 18:19:01 +0200

    update to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,26 @@ const answerButtonsContainer = document.querySelector(".container-bottoni");
 // Tutti i pulsanti delle risposte
 const answerButtons = answerButtonsContainer.querySelectorAll(".bottone");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const START_TIME = 60;
+  const countdownEl = document.getElementById("countdown");
+  let timeLeft = START_TIME;
+
+  countdownEl.textContent = timeLeft;
+
+  const timer = setInterval(() => {
+    timeLeft--;
+
+    if (timeLeft <= 0) {
+      countdownEl.textContent = "0";
+      clearInterval(timer);
+    } else {
+      countdownEl.textContent = timeLeft;
+    }
+  }, 1000);
+});
+
+
 // ARRAY DELLE DOMANDE
 
 const allQuestions = [
